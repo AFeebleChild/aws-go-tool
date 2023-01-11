7058e2bcc57b7c4df5cd347727d40613603f2c40
afeeblechild <afeeblechild@gmail.com>
2023-01-10 21:24:06 -0800

    Add utils.Check

--- lib/utils/logging.go
+++ lib/utils/logging.go
@@ -3,10 +3,18 @@ package utils
 import (
 	"fmt"
 	"log"
+	"os"
 )
 
 //Helper function to print to stdout and the log file
 func LogAll(args ...interface{}) {
 	fmt.Println(args...)
 	log.Println(args...)
+}
+
+func Check(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
